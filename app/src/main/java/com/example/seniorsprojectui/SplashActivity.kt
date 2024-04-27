88b0c3d7c4e2f873cdb885bce0411e4b140668fd
MRUMAR85 <144999591+MRUMAR85@users.noreply.github.com>
2024-04-27 23:47:23 +0500

    1to11

---
+++ app/src/main/java/com/example/seniorsprojectui/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.example.seniorsprojectui
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.seniorsprojectui.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
